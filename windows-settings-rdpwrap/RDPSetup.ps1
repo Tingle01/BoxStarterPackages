63ca9ff9554a9f9b7e01ce6a8d23f6bfe867eac8
Darren Tingle <darren.tingle@sage.com>
2018-10-29 14:37:42 +0000

    Update RDPSetup.ps1

--- windows-settings-rdpwrap/RDPSetup.ps1
+++ windows-settings-rdpwrap/RDPSetup.ps1
@@ -15,31 +15,31 @@ Function Installer ($Installer)
     $obj.SendKeys($Installer)
   } 
 
-  # Set ignore cert errors on RDP
-  $RegPath = "HKLM:\SOFTWARE\Microsoft\Terminal Server Client"
-  Set-ItemProperty $RegPath "AuthenticationLevelOverride" -Value "0" -type DWord
+# Set ignore cert errors on RDP
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Terminal Server Client"
+Set-ItemProperty $RegPath "AuthenticationLevelOverride" -Value "0" -type DWord
 
-  # open Remote Desktop with 'local\administrator'
-  stop-process -processname MicrosoftEdge -Force -ErrorAction SilentlyContinue
-  Start-Sleep -Seconds 10
-  $hostname = "127.0.0.2"
-  
-  Write-Host "starting connection to '$hostname' using '$hostname\SageAdmin' credentials!"
-  cmdkey /delete:"$hostname" # probably not needed, just clears the credentials
-  cmdkey /generic:"$hostname" /user:"$AdminUsername" /pass:"$AdminPassword"
-  Start-Process "$env:windir\system32\mstsc.exe" -ArgumentList "/v:$hostname /w:1280 /h:1024"
-  
-  try
-  {
-    #Maximise RDP
-    Start-Sleep -Seconds 5
-    write-output "send key: % x"
-    Installer {"% x"}
-  }
-  catch
-  {
-    write-host "Error!!!!"
-    Pause
-  }
+# open Remote Desktop with 'local\administrator'
+stop-process -processname MicrosoftEdge -Force -ErrorAction SilentlyContinue
+Start-Sleep -Seconds 10
+$hostname = "127.0.0.2"
 
-  Write-Host "RDP done!"
+Write-Host "starting connection to '$hostname' using '$hostname\SageAdmin' credentials!"
+cmdkey /delete:"$hostname" # probably not needed, just clears the credentials
+cmdkey /generic:"$hostname" /user:"$AdminUsername" /pass:"$AdminPassword"
+Start-Process "$env:windir\system32\mstsc.exe" -ArgumentList "/v:$hostname /w:1280 /h:1024"
+  
+try
+{
+	#Maximise RDP
+	Start-Sleep -Seconds 5
+	write-output "send key: % x"
+	Installer {"% x"}
+}
+catch
+{
+	write-host "Error!!!!"
+	Pause
+}
+
+Write-Host "RDP done!"
