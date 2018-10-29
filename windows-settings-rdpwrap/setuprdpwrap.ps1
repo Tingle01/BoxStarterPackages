ca91cf456cf823e8695df55f3f3f15f15eeef393
Darren Tingle <darren.tingle@sage.com>
2018-10-29 14:36:26 +0000

    Update setuprdpwrap.ps1

--- windows-settings-rdpwrap/setuprdpwrap.ps1
+++ windows-settings-rdpwrap/setuprdpwrap.ps1
@@ -54,58 +54,58 @@ trap
 # Main execution block.
 #
 
-  try
-     {
+try
+	{
 	    #Create New User
-      NET USER "$RDPUsername" "$RDPPassword" /ADD
-      NET LOCALGROUP "Administrators" "$RDPUsername" /add
-      NET LOCALGROUP "Remote Desktop Users" "$RDPUsername" /add
-      Write-Host "Created User $RDPUsername and added to Groups old way @ $(Get-Date)"
-	    
-      #Set-up AutoRun
-      #Registry path declaration
-      $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-      $RegROPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
+	NET USER "$RDPUsername" "$RDPPassword" /ADD
+	NET LOCALGROUP "Administrators" "$RDPUsername" /add
+	NET LOCALGROUP "Remote Desktop Users" "$RDPUsername" /add
+	Write-Host "Created User $RDPUsername and added to Groups old way @ $(Get-Date)"
 
-      #setting registry values
-      Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String  
-      Set-ItemProperty $RegPath "DefaultUsername" -Value "$RDPUsername" -type String  
-      Set-ItemProperty $RegPath "DefaultPassword" -Value "$RDPPassword" -type String
-      Set-ItemProperty $RegPath "AutoLogonCount" -Value "5" -type DWord
-      Set-ItemProperty $RegROPath "(Default)" -Value "C:\RDPSetup\RDPSetup.bat" -type String
-	    Write-Host "Set-up Autorun @ $(Get-Date)"
-            
-	    #Block Network Discovery
-	    reg add "HKLM\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" /f
-	    Write-Host "Blocking Network Discovery"
-	
-	    #Create Startup Script
-	    $RDPPath = "C:\RDPSetup"
-	    New-Item $RDPPath -type directory -force
-	    New-Item "$RDPPath\RDPSetup.bat" -type file -force -value "@echo off
-	    TIMEOUT 120
-	    start /min powershell -executionpolicy bypass $RDPPath\RDPSetup.ps1 -AdminUsername $AdminUsername -AdminPassword $AdminPassword"
-	    
-	    #Copy Files required
-	    Copy-Item .\RDPSetup.ps1 -Destination $RDPPath
-	    Copy-Item .\RDPConf.exe -Destination $RDPPath
-	    Copy-Item .\RDPWInst.exe -Destination $RDPPath
-	    #Copy-Item .\Install.bat -Destination $RDPPath
-	    Write-Host "Created Start-up script for user $RDPUsername @ $(Get-Date)" 
-	    
-	    $winVerString = (Get-WmiObject Win32_OperatingSystem).Caption
-	    Write-Output "Detected Windows Version: $winVerString"
-	    
-      #Install RDP Wrap sourced from https://github.com/stascorp/rdpwrap
-	    Write-Host "Installing Workaround @ $(Get-Date)"
-	    #& .\install.bat
-	    Start-Process -FilePath ".\RDPWInst.exe" -ArgumentList "-i -o" -Verb runas -Wait
-	    Write-Host "Start 180 seconds sleep @ $(Get-Date)"
-	    Start-Sleep -Seconds 180
-	    Write-Host "Artifact Installed @ $(Get-Date)"
+	#Set-up AutoRun
+	#Registry path declaration
+	$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+	$RegROPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce"
+
+	#setting registry values
+	Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String  
+	Set-ItemProperty $RegPath "DefaultUsername" -Value "$RDPUsername" -type String  
+	Set-ItemProperty $RegPath "DefaultPassword" -Value "$RDPPassword" -type String
+	Set-ItemProperty $RegPath "AutoLogonCount" -Value "5" -type DWord
+	Set-ItemProperty $RegROPath "(Default)" -Value "C:\RDPSetup\RDPSetup.bat" -type String
+	Write-Host "Set-up Autorun @ $(Get-Date)"
+
+	#Block Network Discovery
+	reg add "HKLM\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" /f
+	Write-Host "Blocking Network Discovery"
+
+	#Create Startup Script
+	$RDPPath = "C:\RDPSetup"
+	New-Item $RDPPath -type directory -force
+	New-Item "$RDPPath\RDPSetup.bat" -type file -force -value "@echo off
+	TIMEOUT 120
+	start /min powershell -executionpolicy bypass $RDPPath\RDPSetup.ps1 -AdminUsername $AdminUsername -AdminPassword $AdminPassword"
+
+	#Copy Files required
+	Copy-Item .\RDPSetup.ps1 -Destination $RDPPath
+	Copy-Item .\RDPConf.exe -Destination $RDPPath
+	Copy-Item .\RDPWInst.exe -Destination $RDPPath
+	#Copy-Item .\Install.bat -Destination $RDPPath
+	Write-Host "Created Start-up script for user $RDPUsername @ $(Get-Date)" 
+
+	$winVerString = (Get-WmiObject Win32_OperatingSystem).Caption
+	Write-Output "Detected Windows Version: $winVerString"
+
+	#Install RDP Wrap sourced from https://github.com/stascorp/rdpwrap
+	Write-Host "Installing Workaround @ $(Get-Date)"
+	#& .\install.bat
+	Start-Process -FilePath ".\RDPWInst.exe" -ArgumentList "-i -o" -Verb runas -Wait
+	Write-Host "Start 180 seconds sleep @ $(Get-Date)"
+	Start-Sleep -Seconds 180
+	Write-Host "Artifact Installed @ $(Get-Date)"
       }
 
 finally
-{
-  popd
-}
+	{
+	  popd
+	}
